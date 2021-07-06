68b1d0f9eb20e1b2624fb33efcd6b57d2ee79955
yuribasketasia <yuri@basketasia.com>
2021-07-06 16:34:44 +0800

    add register and login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>posty</title>
-
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}"
-
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" >
     </head>
     <body class="bg-gray-200">
             <nav class="p-6 bg-white flex justify-between mb-6">
@@ -21,22 +19,24 @@
                     <a href="" class="p-3">Post</a>
                     </li>
                 </ul> 
-
                 <ul class="flex item-center">
+                    @auth
+                        <li>
+                            <a href="" class="p-3">Yuri</a>
+                        </li>
+                        <li>
+                            <a href="" class="p-3">Logout</a>
+                        </li>
+                    @endauth
+                    @guest
                     <li>
-                    <a href="" class="p-3">Login</a>
+                        <a href="" class="p-3">Login</a>
+                        </li>
+                        <li>
+                        <a href="{{ route('register') }}" class="p-3">Register</a>
                     </li>
-                    <li>
-                    <a href="" class="p-3">Yuri</a>
-                    </li>
-                    <li>
-                    <a href="" class="p-3">Register</a>
-                    </li>
-                    <li>
-                    <a href="" class="p-3">Logout</a>
-                    </li>
+                    @endguest
                 </ul> 
-
             </nav>
         @yield('content')
     </body>
