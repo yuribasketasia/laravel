c2b35b0839620f726ffa7ab9cf6ce561b46f43c4
yuribasketasia <yuri@basketasia.com>
2021-07-06 11:17:56 +0800

    migrate db

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>posty</title>
 
-<h1>This is a Heading</h1>
-@yeild('content')
-</body>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"
+
+    </head>
+    <body class="bg-gray-200">
+            <nav class="p-6 bg-white flex justify-between mb-6">
+                <ul class="flex item-center">
+                    <li>
+                    <a href="" class="p-3">Home</a>
+                    </li>
+                    <li>
+                    <a href="" class="p-3">Dashboard</a>
+                    </li>
+                    <li>
+                    <a href="" class="p-3">Post</a>
+                    </li>
+                </ul> 
+
+                <ul class="flex item-center">
+                    <li>
+                    <a href="" class="p-3">Login</a>
+                    </li>
+                    <li>
+                    <a href="" class="p-3">Yuri</a>
+                    </li>
+                    <li>
+                    <a href="" class="p-3">Register</a>
+                    </li>
+                    <li>
+                    <a href="" class="p-3">Logout</a>
+                    </li>
+                </ul> 
+
+            </nav>
+        @yield('content')
+    </body>
 </html>
