b64fad59b1aec7282b0952f07e1a915edbed4113
yuribasketasia <yuri@basketasia.com>
2021-07-06 12:25:01 +0800

    added register controler model

--- database/migrations/2021_07_06_040840_add_username_to_users_table.php
+++ database/migrations/2021_07_06_040840_add_username_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddUsernameToUserTable extends Migration
+class AddUsernameToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AddUsernameToUserTable extends Migration
      */
     public function up()
     {
-        Schema::table('user', function (Blueprint $table) {
-            //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username');
         });
     }
 
@@ -25,8 +25,8 @@ class AddUsernameToUserTable extends Migration
      */
     public function down()
     {
-        Schema::table('user', function (Blueprint $table) {
-            //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
         });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    #return view('index');
-});
-
+ 
 Route::get('/posts', function () {
     return view('posts.index');
 });
